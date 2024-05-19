ab7f5a677bec808bffd976973ee93f3d3d1d1361
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2024-05-19 16:58:32 +0000

    testing login

--- app/login/page.js
+++ app/login/page.js
@@ -1,3 +1,46 @@
+"use client";
+
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: black;
+`;
+
+const Card = styled.div`
+  margin-top: 100px;
+  width: 370px;
+  height: auto;
+  background: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 10px;
+  padding: 20px;
+`;
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <Container>
+      <Card>
+        <Text>Nome</Text>
+
+        <Input placeholder="Insira o seu nome" type="email" />
+        <Text>Email</Text>
+
+        <Input placeholder="Insira o seu email" type="text" />
+
+        <Text>Senha</Text>
+
+        <Input placeholder="Insira a sua senha" type="password" />
+
+        <Text>Confirmar Senha</Text>
+
+        <Input placeholder="Confirme sua senha" type="password" />
+        <A>Já tenho uma conta</A>
+        <Button>Entrar</Button>
+      </Card>
+    </Container>
+  );
 }
