fcb77ec54ff51e201f3cfd549bac00fecc781bc6
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2024-05-19 17:21:09 +0000

    testing login

--- app/login/page.js
+++ app/login/page.js
@@ -1,20 +1,25 @@
+import "./login.css";
+
 export default function Login() {
   return (
-    <div>
-      <p>Nome</p>
+    <div className="Container">
+      <div className="Card">
+        <p className="text">Nome</p>
 
-      <input placeholder="Insira o seu nome" type="email" />
-      <p>Email</p>
+        <input placeholder="Insira o seu nome" type="email" />
+        <p className="text">Email</p>
 
-      <input placeholder="Insira o seu email" type="text" />
+        <input placeholder="Insira o seu email" type="text" />
 
-      <p>Senha</p>
+        <p className="text">Senha</p>
 
-      <input placeholder="Insira a sua senha" type="password" />
+        <input placeholder="Insira a sua senha" type="password" />
 
-      <p>Confirmar Senha</p>
+        <p className="text">Confirmar Senha</p>
 
-      <input placeholder="Confirme sua senha" type="password" />
+        <input placeholder="Confirme sua senha" type="password" />
+        <button className="button">Entrar</button>
+      </div>
     </div>
   );
 }
